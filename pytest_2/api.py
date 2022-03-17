6be9547f82df5b0c06f16211f79c8f15fd4bcfcf
Polyakov Vladislav <95492562+llllllllllllllllI@users.noreply.github.com>
2022-03-17 15:06:09 +0300

    Add files via upload

---
+++ pytest_2/api.py
@@ -0,0 +1,104 @@
+import json.decoder
+
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+
+class PetFriends:
+    def __init__(self):
+        self.base_url = "https://petfriends1.herokuapp.com/"
+
+    def get_api_key(self, email, password):
+        headers = {
+            "email": email,
+            "password": password
+        }
+        res = requests.get(self.base_url + "api/key", headers=headers)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def get_list_of_pets(self, auth_key: json, filter: str = ""):
+        headers = {"auth_key": auth_key["key"]}
+        filter = {"filter": filter}
+
+        res = requests.get(self.base_url+"api/pets", headers=headers, params=filter)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def add_new_pet(self, auth_key: json, name: str, animal_type: str, age: str, pet_photo: str) -> json:
+        data = MultipartEncoder(
+            fields={
+                "name": name,
+                "animal_type": animal_type,
+                "age": age,
+                "pet_photo": (pet_photo, open(pet_photo, "rb"), "image/jpeg")
+            })
+
+        headers = {"auth_key": auth_key["key"],
+                   "Content_Type": data.content_type}
+
+        res = requests.post(self.base_url + "api/pets", headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
+        return status, result
+
+    def add_photo_of_pet(self, auth_key, pet_id, pet_photo):
+        data = MultipartEncoder(
+            fields= {
+                "pet_id": pet_id,
+                "pet_photo": (pet_photo, open(pet_photo, "rb"), "image/jpeg")
+            }
+        )
+
+        headers = {"auth_key": auth_key["key"],
+                   "Content_Type": data.content_type
+                    }
+        res = requests.post(self.base_url+"api/pets/set_photo" + pet_id, headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
+
+    def delete_pet_from_database(self, auth_key, pet_id):
+        headers = {"auth_key": auth_key["key"],
+                   "pet_id": pet_id
+                   }
+        res = requests.delete(self.base_url+"api/pets/" + pet_id, headers=headers)
+        status = res.status_code
+        result = ""
+        return status
+
+    def update_info_about_pet(self, auth_key, pet_id, name, animal_type, age):
+        headers = {
+            "auth_key": auth_key["key"],
+            "pet_id": pet_id,
+            "name": name,
+            "animal_type": animal_type,
+            "age": age,
+        }
+        res = requests.put(self.base_url+"api/pets" + pet_id, headers=headers)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
---
+++ pytest_2/settings.py
@@ -0,0 +1,2 @@
+valid_email = "f3n1ks1999@gmail.com"
+valid_password = "3sxSHC6VdHBN86x"
