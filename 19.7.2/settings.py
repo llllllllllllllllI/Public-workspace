5961156c5690f13819c2f84885efc13bb2aea0c1
Polyakov Vladislav <95492562+llllllllllllllllI@users.noreply.github.com>
2022-03-21 20:10:22 +0300

    Add files via upload

---
+++ 19.7.2/settings.py
@@ -0,0 +1,5 @@
+valid_email = "test_pet_friend_api@mail.ru"
+valid_password = "123456789"
+invalid_email = "wrng@test.mail.ru"
+invalid_password = "kfkkpwpo"
+invalid_authkey='ea738148a1f19838e1c5d1413877f3691a3731380e733e877b0ae729'
